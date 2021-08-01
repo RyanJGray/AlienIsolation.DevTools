82141c9b4fb4ca9aaaca41fd051f53d16bb5e36d
Ryan J. Gray <ryan@ryanjgray.com>
2021-08-01 22:01:24 +0100

    Add comment briefly describing CATHODE's ShortGuid datatype.

--- CATHODE.h
+++ CATHODE.h
@@ -6,5 +6,8 @@ namespace CATHODE
 	 * \brief Holds a pointer to a CATHODE object in memory.
 	 */
 	typedef unsigned int MemoryPtr;
+	/**
+	 * \brief Holds the unique GUID of a CATHODE node / parameter.
+	 */
 	typedef unsigned int ShortGuid;
 }
